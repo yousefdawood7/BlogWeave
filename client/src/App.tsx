b68edfb85d9836fdda0d4b34522ff822c4b696c7
Yousef Dawood <yousefdawood31@gmail.com>
2026-03-24 21:21:00 +0200

    setup shadcn's darkmode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function App() {
-  return (
-    <div>
-      <Button>HI</Button>
-    </div>
-  );
+  return <main className="flex items-center justify-center min-h-svh"></main>;
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "@/App";
 import { StrictMode } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
 
 import "@/../globals.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
